e9076a9a1fc5a55f8acb0f97cbe3f040cd6db6b6
gongbox <1326114523@qq.com>
2021-06-25 09:00:13 +0800

    模型类修改

--- easyexcel-plus-boot-starter-test/src/main/java/com/gongbo/excel/example/view/ExportDemoView.java
+++ easyexcel-plus-boot-starter-test/src/main/java/com/gongbo/excel/example/view/ExportDemoView.java
@@ -1,6 +1,9 @@
 package com.gongbo.excel.example.view;
 
+import cn.hutool.core.util.RandomUtil;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentRowHeight;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -13,30 +16,34 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Data
+@ColumnWidth(12)
+@ContentRowHeight(18)
 public class ExportDemoView {
 
-    @ExcelProperty
-    private String text = "text";
+    @ExcelProperty("文本")
+    private String text = RandomUtil.randomString(8);
 
-    @ExcelProperty
-    private Integer integerValue = 12;
+    @ExcelProperty("整数")
+    private Integer integerValue = RandomUtil.randomInt(10000);
 
-    @ExcelProperty
-    private Float floatValue = 13.0f;
+    @ExcelProperty("浮点数")
+    private Float floatValue = (float) RandomUtil.randomDouble(-10000, 10000);
 
-    @ExcelProperty
-    private Double doubleValue = 213.4566;
+    @ExcelProperty("长浮点数")
+    private Double doubleValue = RandomUtil.randomDouble(-10000, 10000);
 
-    @ExcelProperty
-    private BigDecimal bigDecimal = BigDecimal.TEN;
+    @ExcelProperty("定点数")
+    private BigDecimal bigDecimal = RandomUtil.randomBigDecimal(BigDecimal.valueOf(10_000));
 
-    @ExcelProperty
+    @ExcelProperty("日期")
     private LocalDate localDate = LocalDate.now();
 
-    @ExcelProperty
+    @ExcelProperty("日期时间")
+    @ColumnWidth(20)
     private LocalDateTime localDateTime = LocalDateTime.now();
 
-    @ExcelProperty
+    @ExcelProperty("时间")
+    @ColumnWidth(20)
     private Date date = new Date();
 
     public static List<ExportDemoView> data() {
